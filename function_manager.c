88ccde635d54a5551f5a0833ecd18122d844677b
Olawale Komolafe <37154613+walpal@users.noreply.github.com>
2022-05-26 08:03:05 +0100

    Update function_manager.c

--- function_manager.c
+++ function_manager.c
@@ -1,28 +1,69 @@
 #include <stdlib.h>
 #include "main.h"
-
+#include <stdarg.h>
 /**
- * *create_array - creates an array of integers
- * @min: minimum range of values stored
- * @max: maximum range of values stored and number of elements
- *
- * Return: pointer to the new array
+ *function_manager - function manager
+ *@c: character to find
+ *@arg: va_list type
+ *Description: This function call other functions
+ * when the character is found
+ *Return: count of printed characters
  */
-char *create_array(unsigned int size, char c)
+int function_manager(char c, va_list arg)
 {
-	int *ptr;
-	int i, size;
+	int cont = 0;
 
-	if (size = 0)
-		return (NULL);
+	cont = _switch(c, arg);
+	if (c == 'p')
+		cont = print_ptr(arg);
+	return (cont);
+}
+/**
+ *_switch - switch
+ *@c: character to find
+ *@arg: va_list type
+ *Description: This function evaluate cases
+ *Return: count of printed characters
+ */
+int _switch(char c, va_list arg)
+{
+	int cont = 0;
 
-	ptr = malloc(sizeof(char) * size);
-
-	if (ptr == NULL)
-		return (NULL);
-
-	for (i = 0; min <= max; i++)
-		ptr[i] = min++;
-
-	return (ptr);
+	switch (c)
+	{
+		case 'b':
+			cont += print_unsign(arg, 2);
+			break;
+		case 'c':
+			cont += print_character(arg);
+			break;
+		case 'd':
+		case 'i':
+			cont += print_sign(arg, 10);
+			break;
+		case 'o':
+			cont += print_unsign(arg, 8);
+			break;
+		case 'r':
+			cont += print_rev(arg);
+			break;
+		case 'R':
+			cont += print_rot13(arg);
+			break;
+		case 's':
+			cont += print_string(arg);
+			break;
+		case 'u':
+			cont += print_unsign(arg, 10);
+			break;
+		case 'x':
+			cont += print_base16_upper_lower(arg, "0123456789abcdef");
+			break;
+		case 'X':
+			cont += print_base16_upper_lower(arg, "0123456789ABCDEF");
+			break;
+		default:
+			cont = -1;
+	}
+	return (cont);
 }
